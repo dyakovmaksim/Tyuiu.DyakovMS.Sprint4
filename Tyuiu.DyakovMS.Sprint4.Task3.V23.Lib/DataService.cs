d4154cc28d27363e8f468c92084fdbec1d928994
dyakovmaksim <pochta3478@mail.ru>
2024-12-09 18:52:41 +0500

    Task3

---
+++ Tyuiu.DyakovMS.Sprint4.Task3.V23.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+
+namespace Tyuiu.DyakovMS.Sprint4.Task3.V23.Lib
+{
+    public class DataService : ISprint4Task3V23
+    {
+        public int Calculate(int[,] array)
+        {
+            int sum = 0;
+            foreach (var item in array) 
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.DyakovMS.Sprint4.Task3.V23.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Tyuiu.DyakovMS.Sprint4.Task3.V23.Lib;
+
+namespace Tyuiu.DyakovMS.Sprint4.Task3.V23.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[,] array =
+{
+                { 7, 5, 5, 6, 7, },
+                { 8, 7, 8, 4, 3, },
+                { 5, 6, 7, 8, 3, },
+                { 4, 2, 3, 6, 4, },
+                { 5, 2, 4, 2, 3, },
+            };
+
+            int result = ds.Calculate(array);
+
+            int wait = 124;
+
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
